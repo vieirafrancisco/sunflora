3fe032731b479780f3b73300cc28a78642e60b07
Francisco Neto <fvcneto.master98@gmail.com>
2021-07-28 17:06:21 -0300

    refactor: remove unused code and update some code

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from sunflower.main import get_reviews_by_product_url, test, save_html
-from sunflower import settings
+# from sunflower.main import get_reviews_by_product_url, test, save_html
+# from sunflower import settings
+from pprint import pprint
+from sunflower.marketplaces.mglu.core import MagazineLuizaSunflower
+from sunflower.db import Database
+from sunflower.db.models import models_list
 
 if __name__ == "__main__":
-    # get_reviews_by_product_url(settings.EXAMPLE_URL)
-    # save_html("test_data", settings.EXAMPLE_URL)
-    test()
+    mglu = MagazineLuizaSunflower()
+    categories = mglu.get_categories()
+    # db = Database()
+    # db.create_tables(models_list)
---
+++ sunflower/__init__.py
@@ -0,0 +1,1 @@
+from .sunflower import Sunflower
