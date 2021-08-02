80200eeac84a9e85d0ed9b72137d27553b9ac312
Francisco Neto <fvcneto.master98@gmail.com>
2021-08-02 18:13:18 -0300

    refactor: update settings.py

--- sunflower/settings.py
+++ sunflower/settings.py
@@ -1,9 +1,29 @@
+import os
 import logging
 from decouple import config
+from requests_cache import CachedSession
 
+# directories
+TMP_DIR = "sunflower/tmp"
+CACHE_DIR = os.path.join(TMP_DIR, "cache")
+
+# debug=True development only
 DEBUG = config("DEBUG", cast=bool, default=False)
 
+# logging
 logging_level = logging.INFO
 if DEBUG:
     logging_level = logging.DEBUG
-logging.basicConfig(level=logging_level, format='[%(levelname)s: %(asctime)s] %(name)s - %(message)s')
+
+logging.basicConfig(
+    level=logging_level, format="[%(levelname)s: %(asctime)s] %(name)s - %(message)s"
+)
+
+# cache
+session = CachedSession(
+    backend="sqlite",  # TODO: use redis
+    namespace="request-cache",
+    allowable_codes=(200,),
+    allowable_methods=("GET",),
+    expire_after=300,
+)
